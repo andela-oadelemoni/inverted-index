f3cb01d551528d07ae4a3e231fa50d83403011c0
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@andela.com>
2016-01-04 23:43:27 +0100

    Add index mapping and index verification test methods

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,16 +1,25 @@
 describe("Inverted index test suite", function() {
 	describe("Read book data", function() {
 		it("should check if json array is empty", function() {
-			expect(BookData.read()).not.toEqual({});
+			expect(BookData.readFile()).toBeTruthy();
 		});
 	});
 
-	/*describe("Populate index", function() {
+	describe("Populate index", function() {
 		it("should verify that index is created", function() {
-			// enter test case here
+			spyOn(BookData, "readFile");
+			BookData.createIndex();
+			expect(BookData.readFile).toHaveBeenCalled();
+			expect(BookData.createIndex()).toBeTruthy();
 		});
-		it("should verify that index is mapped", function() {
-			// enter test case here
+
+		it("should verify that index string key is correctly mapped to JSON object", function() {
+			expect(BookData.verifyMapping("Alice")).toEqual(["book1"]);
+			expect(BookData.verifyMapping("elf")).toEqual(["book2"]);
+			expect(BookData.verifyMapping("and")).toEqual(["book1", "book2"]);
 		});
+	});
+
+	/*describe("Search index", function() {
 	});*/
 });
--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,22 +1,67 @@
 var BookData = {
 
-	read: function() {
-		loadJSON(function(response) {
-			var loadedJSON = JSON.parse(response);
+	fileName: "books.json",
+	bookObj: null,
+	invertedIndex: {},
+
+	readFile: function() {
+		loadJSON(this.fileName, function(response) {
+			bookObj = JSON.parse(response);
 		});
+		return Object.keys(bookObj) !== 0;
+	},
+
+	createIndex: function() {
+		this.readFile();
+		if(bookObj !== null) {
+			for(var i = 0; i < bookObj.length; i++) {
+				var wordsArray = [];
+				var currentBook = "book" + (i + 1);
+				titleStrings = bookObj[i].title.replace(/[^A-Za-z0-9 ]/g, "").split(" ");
+				textStrings = bookObj[i].text.replace(/[^A-Za-z0-9 ]/g, "").split(" ");
+
+				wordsArray.push.apply(wordsArray, titleStrings);
+				wordsArray.push.apply(wordsArray, textStrings);
+
+				for (var word in wordsArray) {
+					var text = wordsArray[word];
+					if (this.invertedIndex.hasOwnProperty(text) && this.invertedIndex[text].indexOf(currentBook) === -1) {
+						this.invertedIndex[text].push(currentBook);
+					}
+					else {
+						this.invertedIndex[text] = [currentBook];
+					}
+				}
+			}
+			return !isEmpty(this.invertedIndex);
+		}
+	},
+
+	verifyMapping: function(key) {
+		return this.invertedIndex[key];
 	}
 };
 
-function loadJSON(callback) {
+/* helper methods */
+
+function loadJSON(fileName, callback) {
 	var xobj = new XMLHttpRequest();
 
 	xobj.overrideMimeType("application/json");
-	xobj.open('GET', 'books.json', true);
+	xobj.open('GET', fileName, false);
 	xobj.onreadystatechange = function() {
-		if (xobj.readyState == 4 && xobj.status == "200") {
+		if (xobj.readyState == 4) {
 			callback(xobj.responseText);
 		}
 	};
 	xobj.send(null);
+}
+
+function isEmpty(obj) {
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop))
+			return false;
+	}
+	return true;
 }
 
